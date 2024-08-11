90aabd32c4ff2518cfd44cdda682d66aa9b49309
gimsangjun <5321913@naver.com>
2024-08-11 16:40:03 +0900

    refactor: 서버 개발환경에 따른 환경변수 설정 변경할수 있게

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,27 @@ async function startServer() {
   const app = express();
 
   // 위와 아래가 같은 표현
+  // TODO: 표현 익히기
   // await require("./loaders").default({ expressApp: app });
   const loader = await import("./loaders");
   await loader.default({ expressApp: app });
 
+  const currentEnvironment = process.env.NODE_ENV;
+  const HOST = currentEnvironment === "production" ? "0.0.0.0" : "localhost";
+
+  Logger.info(`Current NODE_ENV: ${currentEnvironment || "not set"}`); // NODE_ENV가 설정되지 않은 경우 "not set"을 출력
+  Logger.info(`App will listen on ${HOST}`);
+
   app
-    .listen(config.port, () => {
+    .listen(Number(config.port), HOST, () => {
       Logger.info(`
       ################################################
-      🛡️  Server listening on port: ${config.port} 🛡️
+      🛡️  Server listening on port: ${config.port} at ${HOST} 🛡️
       ################################################
     `);
     })
     .on("error", (err) => {
-      Logger.error(err);
+      Logger.error(`Server failed to start: ${err}`);
       process.exit(1);
     });
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,24 +1,31 @@
 import dotenv from "dotenv";
 
-// Set the NODE_ENV to 'development' by default
-// process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
 const envFound = dotenv.config();
 if (envFound.error) {
   // This error should crash whole process
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
+// 개발 환경이 명시적으로 설정되지 않았다면 'development'로 설정
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+// HOST 변수 설정: 프로덕션 환경이면 모든 인터페이스에서 접근 가능하게, 그 외는 localhost
+const HOST = process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1";
+
 export default {
   port: process.env.PORT || 3000,
   databaseURL:
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/blackdesert",
-  sessionKey: process.env.SESSION_KEY || "helloworld",
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGODB_URI ||
+        "mongodb://default-production-uri:27017/blackdesert"
+      : `mongodb://${HOST}:27017/blackdesert`,
+  sessionKey: process.env.SESSION_KEY,
+
   BdoMarketURL: process.env.BDOMARKET_URL,
-  DISCORD_APPLICATION_ID: process.env.DISCORD_APPLICATION_ID || "", // application id, client id와 동일
+  DISCORD_APPLICATION_ID: process.env.DISCORD_APPLICATION_ID, // application id, client id와 동일
   DISCORD_REDIRECT_URI:
     process.env.DISCORD_REDIRECT_URI ||
-    "http://localhost:3000/auth/discord/redirect",
-  DISCORD_APPLICATION_SECRET: process.env.DISCORD_APPLICATION_SECRET || "",
-  DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN || "", // 튜토리얼의 DISCORD_TOKEN
+    `http://localhost:${process.env.PORT || 3000}/auth/discord/redirect`,
+  DISCORD_APPLICATION_SECRET: process.env.DISCORD_APPLICATION_SECRET,
+  DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN, // 튜토리얼의 DISCORD_TOKEN
 };
