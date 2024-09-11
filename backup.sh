bcc9a78e21e00b3ce790f954045cf01177891374
gimsangjun <5321913@naver.com>
2024-09-11 10:15:01 +0900

    fix: mongodb주소 오류 변경

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Define the output directory for backups
+# 현재 폴더에서 
 backup_path="mongodb_backup/"
 
 # Database name
@@ -9,9 +10,10 @@ dbname="blackdesert"
 # MongoDB user and password
 username="tkwk327"
 password="password1234"
-host="127.0.0.0"  # MongoDB 서버 주소 (필요에 따라 수정)
+host="127.0.0.1"  # MongoDB 서버 주소 (필요에 따라 수정)
 port="27017"      # MongoDB 포트 (기본값은 27017)
 
+# 접속 : mongosh "mongodb://tkwk327:password1234@127.0.0.1:27017/blackdesert"
 # URI format
 uri="mongodb://$username:$password@$host:$port/$dbname"
 
